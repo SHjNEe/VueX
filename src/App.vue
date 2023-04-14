1dad74a32a260c55b4414bd7124188576190f61d
SHjNEe <baotrung.jpvn@gmail.com>
2023-04-14 22:20:23 +0700

    ADd

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,16 @@
 <template>
   <div>
-    <base-container title="Vuex" v-if="isAuth">
-      <the-counter></the-counter>
-      <change-counter></change-counter>
-      <favorite-value></favorite-value>
-    </base-container>
-    <base-container title="Auth">
-      <user-auth></user-auth>
-    </base-container>
+    <the-header></the-header>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import BaseContainer from "./components/BaseContainer.vue";
-import TheCounter from "./components/TheCounter.vue";
-import ChangeCounter from "./components/ChangeCounter.vue";
-import FavoriteValue from "./components/FavoriteValue.vue";
-import UserAuth from "./components/UserAuth.vue";
+import TheHeader from "./components/nav/TheHeader.vue";
+
 export default {
   components: {
-    BaseContainer,
-    TheCounter,
-    ChangeCounter,
-    FavoriteValue,
-    UserAuth,
-  },
-  computed: {
-    counter() {
-      return this.$store.state.counter;
-    },
-    isAuth() {
-      return this.$store.getters.userIsAuthenticated;
-    },
+    TheHeader,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,67 +1,15 @@
-import { createApp } from "vue";
-import { createStore } from "vuex";
+import { createApp } from 'vue';
 
-import App from "./App.vue";
+import router from './router.js';
+import store from './store/index.js';
+import App from './App.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
 
-const counterModule = {
-  state() {
-    return {
-      counter: 0,
-      isLoggedIn: false,
-    };
-  },
-  mutations: {
-    increment(state) {
-      state.counter++;
-    },
-    increase(state, payload) {
-      state.counter += payload.value;
-    },
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isLoggedIn;
-    },
-  },
-  actions: {
-    increase(context, payload) {
-      context.commit("increase", payload);
-    },
-    increment(context) {
-      context.commit("increment");
-    },
-    login(context) {
-      context.commit("setAuth", { isLoggedIn: true });
-    },
-    logout(context) {
-      context.commit("setAuth", { isLoggedIn: false });
-    },
-  },
+const app = createApp(App)
 
-  getters: {
-    finalCounter(state) {
-      return state.counter * 2;
-    },
-    normalizedCounter(_, getters) {
-      const finalCounter = getters.finalCounter * 3;
-      if (finalCounter < 0) {
-        return 0;
-      }
-      if (finalCounter > 100) {
-        return 100;
-      }
-      return finalCounter;
-    },
-    userIsAuthenticated(state) {
-      return state.isLoggedIn;
-    },
-  },
-};
-
-const store = createStore({
-  modules: {
-    counterModule,
-  },
-});
-
-const app = createApp(App);
+app.use(router);
 app.use(store);
-app.mount("#app");
+
+app.component('base-badge', BaseBadge);
+
+app.mount('#app');
